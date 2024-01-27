eb02d215f270d2dcd6f4c617a165f7f2f1a4d23d
rafael <rafaelalpes39>
2024-01-27 17:56:50 +0800

    adding some necesarry content

--- pages/index.vue
+++ pages/index.vue
@@ -29,7 +29,7 @@
 </template> -->
 
 <script setup>
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import {
     initAccordions,
     initCarousels,
@@ -44,6 +44,20 @@ import {
     initTooltips
 } from 'flowbite'
 
+// Declare a new variable using an arrow function
+// const  Jogue_Agora = (() => {
+//    console.log('Jogue_Agora')
+// })();
+
+const Jogue_Agora = () => {
+    console.log('Jogue_Agora')
+
+    // Get the modal element by its ID
+    const modal = document.getElementById('small-modal');
+
+    // Toggle the 'hidden' class to show/hide the modal
+    modal.classList.toggle('hidden');
+}
 // initialize components based on data attribute selectors
 onMounted(() => {
     initAccordions();
@@ -59,45 +73,118 @@ onMounted(() => {
     initTooltips();
 })
 </script>
+
 <template>
     <div class="bg-gray-600 h-screen flex flex-col">
         <div class="max-w-lg mx-auto relative">
             <img src="https://sjhygr-5198-ppp.s3.sa-east-1.amazonaws.com/siteadmin/upload/img/1709107893685719042.png"
                 class="bg-blue-950" />
-
-
-
         </div>
-        <div class="flex-1 overflow-y-auto max-w-lg mx-auto scrollbar-style">
+
+        <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+
+            <div id="small-modal" tabindex="-1"
+                class=" z-50 hidden  overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative  mx-auto w-full max-w-md max-h-full">
+                    <!-- Modal content -->
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 border-2 border-white"
+                        style="background-color: rgba(0, 0, 0, 0.750);">
+
+
+
+                        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                               
+                                <tbody>
+                                    <tr
+                                        class="">
+                                       
+                                        <td class="px-6 py-4">
+                                            Silver
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            Laptop
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            $2999
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            <a href="#"
+                                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        </td>
+                                    </tr>
+                                    
+                                </tbody>
+                            </table>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="flex-1 overflow-y-auto max-w-lg mx-auto scrollbar-style"> -->
+        <div class="flex-1 overflow-y-auto max-w-lg mx-auto scrollbar-style relative">
             <div class="pt-4 bg-blue-950"></div>
             <!-- Your scrollable content here -->
+
+
             <img src="https://sjhygr-5198-ppp.s3.sa-east-1.amazonaws.com/siteadmin/upload/img/1708812659034382338.png" />
             <img src="https://sjhygr-5198-ppp.s3.sa-east-1.amazonaws.com/siteadmin/upload/img/1708812725187829762.png" />
             <img src="https://sjhygr-5198-ppp.s3.sa-east-1.amazonaws.com/siteadmin/upload/img/1708812862310969346.png" />
 
-            <div class="fixed bottom-0 bg-black py-6 px-8 shadow-md space-x-6" style="background-color: rgba(0, 0, 0, 0.712);">
-                <button
-                    class="relative inline-flex items-center justify-center  p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-xl group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+
+            <div class="fixed bottom-0 bg-black py-6 px-6 shadow-md flex space-x-8 sm:space-x-11 md:space-x-11 lg:space-x-11"
+                style="background-color: rgba(0, 0, 0, 0.750);">
+                <button @click="Jogue_Agora"
+                    class="flex-grow relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg">
                     <span
-                        class="relative px-12 py-3 transition-all ease-in duration-75 bg-blue-600 dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-neutral-50 font-semibold text-lg">
+                        class="relative px-12 py-3 transition-all ease-in duration-75 bg-blue-600 dark:bg-gray-900 rounded-md text-neutral-50 font-normal text-xl">
                         Jogue Agora
                     </span>
                 </button>
 
                 <button
-                    class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-xl group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
+                    class="flex-grow relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-xl group">
                     <span
-                        class="relative px-12 py-3 transition-all ease-in duration-75 bg-yellow-400 dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-neutral-50 font-semibold text-lg">
-                        Red to Yellow
+                        class="relative px-12 py-3 transition-all ease-in duration-75 bg-yellow-400 dark:bg-gray-900 rounded-md text-neutral-50 font-normal text-xl">
+                        Baixe Agora
                     </span>
-                </button>   
+                </button>
             </div>
+
+
+            <!-- <div class="fixed bottom-0 left-50 right-40 bg-black py-6 px-6 shadow-md space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-9"
+                style="background-color: rgba(0, 0, 0, 0.712);">
+                <button @click="Jogue_Agora"
+                    class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm sm:text-base font-medium text-gray-900 rounded-lg">
+                    <span
+                        class="relative px-6 sm:px-8 py-2 sm:py-3 transition-all ease-in duration-75 bg-blue-600 dark:bg-gray-900 rounded-md text-neutral-50 font-normal text-md sm:text-xl">
+                        Jogue Agora
+                    </span>
+                </button>
+
+                <button
+                    class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm sm:text-base font-medium text-gray-900 rounded-xl group">
+                    <span
+                        class="relative px-6 sm:px-8 py-2 sm:py-3 transition-all ease-in duration-75 bg-yellow-400 dark:bg-gray-900 rounded-md text-neutral-50 font-normal text-md sm:text-xl">
+                        Baixe Agora
+                    </span>
+                </button>
+            </div> -->
+
 
 
         </div>
     </div>
 </template>
-
+ <!-- <button
+                    class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-xl group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
+                    <span
+                        class="relative px-12 py-3 transition-all ease-in duration-75 bg-yellow-400 dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-neutral-50 font-semibold text-lg">
+                        Red to Yellow
+                    </span>
+                </button>    -->
 <style>
 .scrollbar-style::-webkit-scrollbar {
     width: 6px;
@@ -114,7 +201,6 @@ onMounted(() => {
 .scrollbar-style::-webkit-scrollbar-track {
     background-color: #2d3748;
     /* Color of the track */
-}
-</style>
+}</style>
 
 
